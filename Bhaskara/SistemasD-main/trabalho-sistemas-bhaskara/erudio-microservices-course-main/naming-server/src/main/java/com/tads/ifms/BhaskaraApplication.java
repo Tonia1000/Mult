e56c39d3905661f3fd2cdf3dc65e96edcc915774
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-11-30 00:21:31 -0400

    c

--- Bhaskara/SistemasD-main/trabalho-sistemas-bhaskara/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/BhaskaraApplication.java
+++ Bhaskara/SistemasD-main/trabalho-sistemas-bhaskara/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/BhaskaraApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class NamingServerApplication {
+public class BhaskaraApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NamingServerApplication.class, args);
+        SpringApplication.run(BhaskaraApplication.class, args);
     }
 
 }
