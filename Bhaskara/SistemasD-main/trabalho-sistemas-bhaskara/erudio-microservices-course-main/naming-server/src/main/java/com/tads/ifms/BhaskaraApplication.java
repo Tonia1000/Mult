0b317d12d5edc1a6f646da332174f89007bf3b5e
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-11-30 00:20:42 -0400

    commit

--- Bhaskara/SistemasD-main/trabalho-sistemas-bhaskara/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/BhaskaraApplication.java
+++ Bhaskara/SistemasD-main/trabalho-sistemas-bhaskara/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/BhaskaraApplication.java
@@ -1,4 +1,4 @@
-package com.neogedom.namingserver;
+package com.tads.ifms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
