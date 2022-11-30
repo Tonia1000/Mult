8f89e28d41aacedb0615151506966d4dd4da3b79
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-11-30 00:17:00 -0400

    com

--- Bhaskara/SistemasD-main/erudio-microservices-course-main/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/controller/MultController.java
+++ Bhaskara/SistemasD-main/erudio-microservices-course-main/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/controller/MultController.java
@@ -1,18 +1,27 @@
 package com.tads.ifms.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GettMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
 @RequestMapping(value = "/mult")
 public class MultController {
 
     @GettMapping (value = "/{x}/{y}")
-    public int GetMult(@PathVariable int x, @PathVariable int y){
+    public int getMult(@PathVariable int x, @PathVariable int y){
         return x*y;
 
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public int soma(@RequestBody Bhaskara bhaskara){
+    public int mult(@RequestBody Bhaskara bhaskara){
         int result;
         result = (bhaskara.getX() + bhaskara.getY());
         return result;
