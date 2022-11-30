c18ef2441172cb95031f4c4d1a38c31cacd7c25e
Tonia1000 <99226928+Tonia1000@users.noreply.github.com>
2022-11-30 00:17:37 -0400

    com

--- Bhaskara/SistemasD-main/erudio-microservices-course-main/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/controller/MultController.java
+++ Bhaskara/SistemasD-main/erudio-microservices-course-main/erudio-microservices-course-main/naming-server/src/main/java/com/tads/ifms/controller/MultController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GettMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping(value = "/mult")
 public class MultController {
 
-    @GettMapping (value = "/{x}/{y}")
+    @GetMapping (value = "/{x}/{y}")
     public int getMult(@PathVariable int x, @PathVariable int y){
         return x*y;
 
